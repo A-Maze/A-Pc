a899fbdceaeca85ff1a43ebfea5db60a54f113cc
Robin Siep <robin@robin-desktop.(none)>
2014-09-28 20:06:31 +0200

    final computerland version

--- Crawlers/pccrawler/pccrawler/spiders/computerland.py
+++ Crawlers/pccrawler/pccrawler/spiders/computerland.py
@@ -13,17 +13,17 @@ class BobSpider(CrawlSpider):
     start_urls = (
         'http://www.computerland.nl',
 
-        
+
     )
-    
+
     rules = (
 
-    Rule(LinkExtractor(restrict_xpaths =('//div[contains(concat(" ", normalize-space(@class), "  "), " dropdown_3columns no_padding_top dropdown_container ")][1]/ul/li/a', )),callback='parse_item',follow=True),
+    Rule(LinkExtractor(restrict_xpaths =('//div/div/div/div[contains(concat(" ", normalize-space(@class), "  "), "megamenu_container megamenu_dark_bar megamenu_light")]/ul[contains(concat(" ", normalize-space(@class), "  "), "megamenu")]/li[2]/div/ul/li/a', )),callback='parse_item',follow=True),
     Rule(LinkExtractor(restrict_xpaths =('//a[contains(concat(" ", normalize-space(@class), "  "), " PagerHyperlinkStyle ")]', )),callback='parse_item',follow=True),
     )
 
- 
-    
+
+
     def parse_item(self, response):
 
         for sel in response.xpath('//div[contains(concat(" ", normalize-space(@class), "  "), " itemlistcombined-productrowcontainer-210 ")]'):
@@ -35,12 +35,12 @@ class BobSpider(CrawlSpider):
             item['categorie'] = sel.xpath('//a[contains(concat(" ", normalize-space(@class), " "), " activebreadcrumbhyperlink ")]/@title').extract()
             item['prijs'] = sel.xpath('div[contains(concat(" ", normalize-space(@class), " "), "itemlistcombined-salespriceincludingvatcontainer")]/div/meta[2]/@content').extract()
             yield item
-            
-  
 
 
 
-            
-       
 
- 
+
+
+
+
+
