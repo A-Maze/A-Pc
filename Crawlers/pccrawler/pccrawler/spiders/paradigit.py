3ce8415507e861dd9246fdbc11ed3d2a3bdfd1a1
Sander de Winter <sanderdewinter@gmail.com>
2014-09-29 08:54:14 +0200

    werkt alleen de naam

--- Crawlers/pccrawler/pccrawler/spiders/paradigit.py
+++ Crawlers/pccrawler/pccrawler/spiders/paradigit.py
@@ -11,17 +11,19 @@ class ParadigitSpider(CrawlSpider):
     name = "paradigit"
     allowed_domains = ["www.paradigit.nl"]
     start_urls = (
-        "http://www.paradigit.nl/catalog/zpr_08ond/06_pccase/default.aspx",
+        "http://www.paradigit.nl",
     )
 
     rules = (
-        Rule(LinkExtractor(restrict_xpaths=("//div[contains(concat(' ', normalize-space(@class), ' '), ' itemlistcombined-toppagercontainer ')]/a[contains(text(),' > ')]", )), callback='parse_item', follow=True),
+        #Rule(LinkExtractor(restrict_xpaths =('//div[@id="ctl00_mainMenuContentContainer"]/div/ul/li[6]/div/div[0]/ul/li/a', )),callback='parse_item',follow=True),
+        Rule(LinkExtractor(restrict_xpaths=('//*[@id="ctl00_mainMenuContentContainer"]/div/ul/li[6]/div/div[1]/ul/li/a', )),callback='parse_item',follow=True),
+        Rule(LinkExtractor(restrict_xpaths=("//div[contains(concat(' ', normalize-space(@class), ' '), ' itemlistcombined-toppagercontainer ')]/table/tr/td/a[contains(text(),' > ')]", )), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
       
         for sel in response.xpath("//div[contains(concat(' ', normalize-space(@class), ' '), ' itemlistcombined-productrowcontainer-210 ')]"):
             item = ParadigitItem()
-            item['naam'] = sel.xpath('div/div[contains(concat(" ", normalize-space(@class), " "), " itemlistcombined-titlecontainer ")]/a/span//text()').extract()[0]
+            item['naam'] = sel.xpath('div/div[contains(concat(" ", normalize-space(@class), " "), " itemlistcombined-titlecontainer ")]/a/span/text()').extract()[0]
 
             yield item
