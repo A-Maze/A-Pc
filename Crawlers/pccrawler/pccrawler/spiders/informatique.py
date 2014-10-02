1861ce22efd59069da39738a8cc3070e147bfc26
Melloww <RobinSiep1@gmail.com>
2014-10-02 12:52:32 +0200

    informatique edit

--- Crawlers/pccrawler/pccrawler/spiders/informatique.py
+++ Crawlers/pccrawler/pccrawler/spiders/informatique.py
@@ -11,7 +11,22 @@ class InformatiqueSpider(CrawlSpider):
     name = "informatique"
     allowed_domains = ["www.informatique.nl"]
     start_urls = (
-        'http://www.informatique.nl/componenten/',
+        'http://www.informatique.nl/?M=GRP&H=007',
+        'http://www.informatique.nl/?M=GRP&H=033',
+        'http://www.informatique.nl/?M=GRP&H=057',
+        'http://www.informatique.nl/?M=GRP&H=020',
+        'http://www.informatique.nl/?M=GRP&H=012',
+        'http://www.informatique.nl/?M=GRP&H=110',
+        'http://www.informatique.nl/?M=USL&G=559',
+        'http://www.informatique.nl/?M=GRP&H=037',
+        'http://www.informatique.nl/?M=GRP&H=021',
+        'http://www.informatique.nl/?M=GRP&H=074',
+        'http://www.informatique.nl/?M=GRP&H=017',
+        'http://www.informatique.nl/?M=GRP&H=008',
+        'http://www.informatique.nl/?M=GRP&H=082',
+        'http://www.informatique.nl/?M=GRP&H=062',
+        'http://www.informatique.nl/?M=GRP&H=011',
+
 
 
     )
@@ -32,7 +47,7 @@ class InformatiqueSpider(CrawlSpider):
             item['subnaam'] = sel.xpath('div[@id="title"]/a/text()').extract()
             item['info'] = sel.xpath('div[@id="description"]/ul/li/text()').extract()
             item['stock'] = sel.xpath('div[@id="stock"]/text()').extract()
-            item['categorie'] = sel.xpath('div[@id="wrapper_content"]/div[@id="hdr"]/h1/text()').extract()
+            item['categorie'] = sel.xpath('//div[@id="wrapper_content"]/div[@id="hdr"]/h1/text()').extract()
             item['prijs'] = sel.xpath('div[@id="price"]/text()').extract()
             yield item
 
