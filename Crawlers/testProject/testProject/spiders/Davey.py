5c0dd10453fe3819945f03a5579a89a7b5dbf52a
drwitter <davey.witter@live.nl>
2014-10-06 20:26:16 +0200

    verbeterde azerty

--- Crawlers/testProject/testProject/spiders/Davey.py
+++ Crawlers/testProject/testProject/spiders/Davey.py
@@ -6,25 +6,25 @@ from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors import LinkExtractor
 
-class Davey(scrapy.Spider):
+class Davey(CrawlSpider):
 	name = "Davey"
 	allowed_domains = ["http://www.azerty.nl"]
 	start_urls = (
-        'http://www.azerty.nl',
+        'http://azerty.nl/8-5824/-met-cpu.html',
     )
     
 	rules = (
 
-    Rule(LinkExtractor(restrict_xpaths =('//div[contains(concat(" ", normalize-space(@class), "  "), " menu ")]/ul/li/ul/[contains(concat(" ", normalize-space(@class), "  "), " leaf ")]/a', )),callback='parse_item',follow=True),
+    Rule(LinkExtractor(restrict_xpaths =('/html/body/div[2]/div[5]/div/ul/li/ul/li[4]/ul/li[1]/ul/li[1]/a', )),callback='parse_item',follow=True),
     #Rule(LinkExtractor(restrict_xpaths =('//div[contains(concat(" ", normalize-space(@class), "  "), " articleSizePerSite ")]/a', )),callback='parse_item',follow=True),
     )
 
-	def parse(self, response):
+	def parse_item(self, response):
 		for sel in response.xpath('//*[contains(concat(" ", normalize-space(@class), "  "), " outer ")]'):
 			item = TestprojectItem()
-			item['titel'] = sel.xpath('ul/li/a/h3[contains(concat(" ", normalize-space(@class), " "), "")]/text()').extract()[0]
+			item['titel'] = sel.xpath('ul/li/a/h3/text()').extract()[0]
 			item['omschrijving'] = sel.xpath('ul/li/ul[contains(concat(" ", normalize-space(@class), " "), "kenmerken ")]/li/text()').extract()[0]
 			item['prijs'] = sel.xpath('ul/li/div/span[contains(concat(" ", normalize-space(@class), " "), "prijs_zicht ")]/text()').extract()[0]
-			item['image'] = sel.xpath('ul/li/a[contains(concat(" ", normalize-space(@class), " "), " ")]/img/@src').extract()[0]
-			item['categorie'] = sel.xpath('//div[@id="zoek-titel"]/h1[contains(concat(" ", normalize-space(@class), " "), "")]/text()').extract()[0]
+			item['image'] = sel.xpath('ul/li/a/img/@src').extract()[0]
+			item['categorie'] = sel.xpath('//div[@id="zoek-titel"]/h1/text()').extract()[0]
 			yield item
