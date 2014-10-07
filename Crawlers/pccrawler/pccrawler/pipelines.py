ddc3e4ea8e33503eef14be2e1814bfb01fdceb9d
Melloww <RobinSiep1@gmail.com>
2014-10-07 14:49:50 +0200

    pipeline

--- Crawlers/pccrawler/pccrawler/pipelines.py
+++ Crawlers/pccrawler/pccrawler/pipelines.py
@@ -8,11 +8,21 @@
 
 class PccrawlerPipeline(object):
     def __init__(self):
-        self.names_seen = set()
-
-    def process_item(self, item, spider):
-        if item['name'] in self.names_seen:
-            raise DropItem("Duplicate item found: %s" % item)
-        else:
-            self.names_see .add(item['name'])
-            return item
+		connection = pymongo.Connection(settings['MONGODB_SERVER'], settings['MONGODB_PORT'])
+		db = connection[settings['MONGODB_DB']]
+		self.collection = db[settings['MONGODB_COLLECTION']]
+        
+	def process_item(self, item, spider):
+		valid = True
+		for data in item:
+          # here we only check if the data is not null
+          # but we could do any crazy validation we want
+		  if not data:
+		    valid = False
+		    raise DropItem("Missing %s of blogpost from %s" %(data, item['url']))
+		if valid:
+		  self.collection.insert(dict(item))
+		  log.msg("Item wrote to MongoDB database %s/%s" %
+		          (settings['MONGODB_DB'], settings['MONGODB_COLLECTION']),
+		          level=log.DEBUG, spider=spider) 
+		return item
