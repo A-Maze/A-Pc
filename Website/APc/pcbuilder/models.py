d57e4cf400dc7381438a46bf34b53f605f76b86e
Sander de Winter <sanderdewinter@gmail.com>
2014-10-11 17:07:54 +0200

    Half werkende shit

--- Website/APc/pcbuilder/models.py
+++ Website/APc/pcbuilder/models.py
@@ -1,14 +1,37 @@
 from django.db import models
+from mongoengine import *
+from django.conf import settings
+result = connect(settings.DBNAME)
+
+
+class Processor(models.Model):
+    categorie = models.CharField(max_length=120)
+    naam = models.CharField(max_length=500)
+    subnaam = models.CharField(max_length=500)
+    info = models.CharField(max_length=500)
+    stock = models.CharField(max_length=500)
+    prijs = models.CharField(max_length=120)
+
+
+
+'''
+from django.db import models
 
 from mongoengine import *
-from APc.settings import DBNAME
+from django.conf import settings
+import logging
 
-connect(DBNAME)
+logging.info(settings.DBNAME)
+
+result = connect(settings.DBNAME)
+logging.info(result)
 
 class Processor(Document):
-	categorie = StringField(max_length=120, required=True)
-	naam = StringField(max_length=500, required=True)
-	subnaam = StringField(max_length=500, required=True)
-	info = StringField(max_length=500, required=True)
-	stock = StringField(max_length=500, required=True)
-   	prijs = StringField(max_length=120, required=True)
+    categorie = StringField(max_length=120, required=True)
+    logging.info(categorie)
+    naam = StringField(max_length=500, required=True)
+    subnaam = StringField(max_length=500, required=True)
+    info = StringField(max_length=500, required=True)
+    stock = StringField(max_length=500, required=True)
+    prijs = StringField(max_length=120, required=True)
+'''
--- Website/APc/pcbuilder/views.py
+++ Website/APc/pcbuilder/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from models import Processor
-import datetime
+import logging
+from pprint import pprint
 
 def index(request):
     # Get all posts from DB
@@ -12,7 +13,24 @@ def index(request):
 def processoren(request):
 
     # Get all posts from DB
-    processoren = Processor.objects(categorie__contains="Processor") 
+    processoren = Processor()
+    logging.info(type(processoren))
+    pprint(vars(processoren))
+    #processoren = Processor.objects(categorie__contains="Processor")
+    #logging.info(processoren)
     return render_to_response('processoren.html', {'Processoren': processoren},
                               context_instance=RequestContext(request))
 
+
+
+
+'''
+ return render_to_response('processoren.html', {'Processoren': processoren},
+                              context_instance=RequestContext(request))
+
+
+
+    logging.info(processoren.naam('Upgrade Kit ASUS MAXIMUS VI HERO / i7 4770K / 8GB'))
+
+
+'''
