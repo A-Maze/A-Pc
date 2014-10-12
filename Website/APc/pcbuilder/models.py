8310197deccc50b1dd6c4c61969d1b5c45bc3476
Sander de Winter <sanderdewinter@gmail.com>
2014-10-12 17:04:54 +0200

    FIX FIX FIX :)

--- Website/APc/pcbuilder/models.py
+++ Website/APc/pcbuilder/models.py
@@ -5,36 +5,12 @@ from APc.settings import DBNAME
 
 connect(DBNAME)
 
-class Processor(Document):
+class Processoren(Document):
     categorie = StringField(max_length=120, required=True)
-    naam = StringField(max_length=500, required=True)
-
-
-'''   subnaam = StringField(max_length=500)
-   info = StringField(max_length=500)
-   stock = StringField(max_length=500)
-   prijs = StringField(max_length=120)
-
-
-
-
-from django.db import models
-
-from mongoengine import *
-from django.conf import settings
-import logging
-
-logging.info(settings.DBNAME)
-
-result = connect(settings.DBNAME)
-logging.info(result)
-
-class Processor(Document):
-    categorie = StringField(max_length=120, required=True)
-    logging.info(categorie)
     naam = StringField(max_length=500, required=True)
     subnaam = StringField(max_length=500, required=True)
     info = StringField(max_length=500, required=True)
     stock = StringField(max_length=500, required=True)
     prijs = StringField(max_length=120, required=True)
-'''
+
+
--- Website/APc/pcbuilder/views.py
+++ Website/APc/pcbuilder/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from models import Processor
+from models import Processoren
 import logging
 
 def index(request):
     # Get all posts from DB
-    processoren = Processor.objects
+    processoren = Processoren.objects
     return render_to_response('index.html', {'Processoren': processoren},
                               context_instance=RequestContext(request))
 
@@ -14,22 +14,7 @@ def index(request):
 def processoren(request):
 
     # Get all posts from DB
-    processoren = Processor.objects
-    logging.info(Processor.title)
+    processoren = Processoren.objects[:5]
     return render_to_response('processoren.html', {'Processoren': processoren},
                               context_instance=RequestContext(request))
 
-
-
-'''
-
-
-return render_to_response('processoren.html', {'Processoren': processoren},
-                              context_instance=RequestContext(request))
-
-
-
-    logging.info(processoren.naam('Upgrade Kit ASUS MAXIMUS VI HERO / i7 4770K / 8GB'))
-
-
-'''
