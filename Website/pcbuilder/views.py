82681fac5c20b33f2a33b9a73d8188f7c1699586
Sander de Winter <sanderdewinter@gmail.com>
2014-10-15 16:57:18 +0200

    Aantal per pagina en pagina combineren met GET parameters

--- Website/pcbuilder/views.py
+++ Website/pcbuilder/views.py
@@ -5,6 +5,11 @@ from models import Processoren
 import logging
 import json
 
+# Global vars
+
+# Strandaard aantal per pagina
+app = 15
+
 def index(request):
     # Get all posts from DB
     processoren = Processoren.objects
@@ -16,7 +21,10 @@ def index(request):
 def processoren(request):
 
     # Get all posts from DB
-    processoren = Processoren.objects.filter(categorie__contains='Processor')[:10]
+    # Aantal per pagina en pagina nummer
+    start, end = limits(request)
+
+    processoren = Processoren.objects.filter(categorie__contains='Processor')[start:end]
     #processoren = json.dumps(list(uniArray))
     return render_to_response('processoren.html', {'Processoren': processoren},
                               context_instance=RequestContext(request))
@@ -24,60 +32,92 @@ def processoren(request):
 def behuizingen(request):
 
     # Get all posts from DB
-    processoren = Processoren.objects(categorie__contains='Behuizing')[:10]
+    # Aantal per pagina en pagina nummer
+    start, end = limits(request)
+
+    processoren = Processoren.objects(categorie__contains='Behuizing')[start:end]
     return render_to_response('behuizing.html', {'Processoren': processoren},
                               context_instance=RequestContext(request))
 
 def geheugen(request):
 
     # Get all posts from DB
-    processoren = Processoren.objects(categorie__contains='Geheugen')[:10]
+    # Aantal per pagina en pagina nummer
+    start, end = limits(request)
+
+    processoren = Processoren.objects(categorie__contains='Geheugen')[start:end]
     return render_to_response('geheugen.html', {'Processoren': processoren},
                               context_instance=RequestContext(request))
 
 def gpu(request):
 
     # Get all posts from DB
-    processoren = Processoren.objects(categorie__contains='Grafische kaarten')[:10]
+    # Aantal per pagina en pagina nummer
+    start, end = limits(request)
+
+    processoren = Processoren.objects(categorie__contains='Grafische kaarten')[start:end]
     return render_to_response('gpu.html', {'Processoren': processoren},
                               context_instance=RequestContext(request))
 
 def hardeschijf(request):
 
     # Get all posts from DB
-    processoren = Processoren.objects(categorie__contains='Harde schijven')[:10]
+    # Aantal per pagina en pagina nummer
+    start, end = limits(request)
+
+    processoren = Processoren.objects(categorie__contains='Harde schijven')[start:end]
     return render_to_response('hardeschijf.html', {'Processoren': processoren},
                               context_instance=RequestContext(request))
 
 def koeling(request):
 
     # Get all posts from DB
-    processoren = Processoren.objects(categorie__contains='Koeling')[:10]
+    # Aantal per pagina en pagina nummer
+    start, end = limits(request)
+
+    processoren = Processoren.objects(categorie__contains='Koeling')[start:end]
     return render_to_response('koeling.html', {'Processoren': processoren},
                               context_instance=RequestContext(request))
 
 def moederborden(request):
 
     # Get all posts from DB
-    aantal = request.GET.get('aantal', '10')
-    logging.info(aantal)
-    aantal = int(aantal)
-    processoren = Processoren.objects(categorie__contains='Moederborden')[:aantal]
+    # Aantal per pagina en pagina nummer
+    start, end = limits(request)
+
+    processoren = Processoren.objects(categorie__contains='Moederborden')[start:end]
     return render_to_response('moederbord.html', {'Processoren': processoren},
                               context_instance=RequestContext(request))
 
 def optischeschijf(request):
 
     # Get all posts from DB
-    processoren = Processoren.objects(categorie__contains='DVD, Blu-ray & backup')[:10]
+    # Aantal per pagina en pagina nummer
+    start, end = limits(request)
+
+    processoren = Processoren.objects(categorie__contains='DVD, Blu-ray & backup')[start:end]
     return render_to_response('optischeschijf.html', {'Processoren': processoren},
                               context_instance=RequestContext(request))
 
 def voedingen(request):
 
     # Get all posts from DB
-    processoren = Processoren.objects(categorie__contains='Voeding')[:10]
+    # Aantal per pagina en pagina nummer
+    start, end = limits(request)
+
+    processoren = Processoren.objects(categorie__contains='Voeding')[start:end]
     return render_to_response('voeding.html', {'Processoren': processoren},
                               context_instance=RequestContext(request))
 
+def pages(request):
+    aantal = request.GET.get('aantal', app)
+    pagina = request.GET.get('pagina', '1')
+    aantal = int(aantal)
+    pagina = int(pagina) - 1
+    return aantal, pagina
 
+def limits(request):
+    app, pagina = pages(request)
+    start = app * pagina
+    end = app * pagina + app
+    return start, end
