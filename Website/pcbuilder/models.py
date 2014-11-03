80d7c138af77d4edeb0e0f06b71035b22db36e9b
Melloww <RobinSiep1@gmail.com>
2014-11-03 10:33:50 +0100

    WADUP

--- Website/pcbuilder/models.py
+++ Website/pcbuilder/models.py
@@ -5,6 +5,14 @@ from APc.settings import DBNAME
 
 connect(DBNAME)
 
+class Moederborden(Document):
+    categorie = StringField(max_length=120)
+    naam = StringField(max_length=500)
+    subnaam = StringField(max_length=500)
+    info = StringField(max_length=500)
+    stock = StringField(max_length=500)
+    prijs = StringField(max_length=120)
+
 class Processoren(Document):
     categorie = StringField(max_length=120)
     naam = StringField(max_length=500)
@@ -13,3 +21,58 @@ class Processoren(Document):
     stock = StringField(max_length=500)
     prijs = StringField(max_length=120)
 
+class Koeling(Document):
+    categorie = StringField(max_length=120)
+    naam = StringField(max_length=500)
+    subnaam = StringField(max_length=500)
+    info = StringField(max_length=500)
+    stock = StringField(max_length=500)
+    prijs = StringField(max_length=120)
+
+class Behuizingen(Document):
+    categorie = StringField(max_length=120)
+    naam = StringField(max_length=500)
+    subnaam = StringField(max_length=500)
+    info = StringField(max_length=500)
+    stock = StringField(max_length=500)
+    prijs = StringField(max_length=120)
+
+class Grafische(Document):
+    categorie = StringField(max_length=120)
+    naam = StringField(max_length=500)
+    subnaam = StringField(max_length=500)
+    info = StringField(max_length=500)
+    stock = StringField(max_length=500)
+    prijs = StringField(max_length=120)
+
+class Harde(Document):
+    categorie = StringField(max_length=120)
+    naam = StringField(max_length=500)
+    subnaam = StringField(max_length=500)
+    info = StringField(max_length=500)
+    stock = StringField(max_length=500)
+    prijs = StringField(max_length=120)
+
+class Dvd(Document):
+    categorie = StringField(max_length=120)
+    naam = StringField(max_length=500)
+    subnaam = StringField(max_length=500)
+    info = StringField(max_length=500)
+    stock = StringField(max_length=500)
+    prijs = StringField(max_length=120)
+
+class Geheugen(Document):
+    categorie = StringField(max_length=120)
+    naam = StringField(max_length=500)
+    subnaam = StringField(max_length=500)
+    info = StringField(max_length=500)
+    stock = StringField(max_length=500)
+    prijs = StringField(max_length=120)
+
+class Voeding(Document):
+    categorie = StringField(max_length=120)
+    naam = StringField(max_length=500)
+    subnaam = StringField(max_length=500)
+    info = StringField(max_length=500)
+    stock = StringField(max_length=500)
+    prijs = StringField(max_length=120)
