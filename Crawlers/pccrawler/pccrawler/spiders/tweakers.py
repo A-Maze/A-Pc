d452ba923118df57d04a576421e8f7bfecc8196a
Melloww <Melloww@github.com>
2014-11-17 20:59:41 +0100

    gpu

--- Crawlers/pccrawler/pccrawler/spiders/tweakers.py
+++ Crawlers/pccrawler/pccrawler/spiders/tweakers.py
@@ -38,6 +38,43 @@ class TweakersSpider(CrawlSpider):
             category = sel.xpath('//*[@id="tweakbaseBreadcrumbCategory"]/a/text()').extract()
             print category
             if "Videokaarten" in category:
+                item = GPU()
+
+                item['Categorie'] = sel.xpath().extract()
+                item['Merk'] = sel.xpath().extract()
+                item['Product'] = sel.xpath().extract()
+                item['Uitvoering'] = sel.xpath().extract()
+                item['Afbeelding'] = sel.xpath().extract()
+                item['Videochip'] = sel.xpath().extract()
+                item['Chipset_generatie'] = sel.xpath().extract()
+                item['Videochipfabrikant'] = sel.xpath().extract()
+                item['Nominale_snelheid_videochip'] = sel.xpath().extract()
+                item['Maximale_turbo_frequentie'] = sel.xpath().extract()
+                item['Rekenkernen'] = sel.xpath().extract()
+                item['Geheugengrootte'] = sel.xpath().extract()
+                item['Geheugen_Type'] = sel.xpath().extract()
+                item['Geheugen_Nominale_snelheid_videochip'] = sel.xpath().extract()
+                item['Geheugen_Busbreedte'] = sel.xpath().extract()
+                item['Card_Interface'] = sel.xpath().extract()
+                item['Video_uit'] = sel.xpath().extract()
+                item['Hoogste_HDMI_versie'] = sel.xpath().extract()
+                item['Hoogste_DisplayPort_versie'] = sel.xpath().extract()
+                item['Video_Adapter'] = sel.xpath().extract()
+                item['DirectX_versie'] = sel.xpath().extract()
+                item['OpenGL_versie'] = sel.xpath().extract()
+                item['Shader_model'] = sel.xpath().extract()
+                item['Lengte'] = sel.xpath().extract()
+                item['Hoogte'] = sel.xpath().extract()
+                item['Breedte'] = sel.xpath().extract()
+                item['Aantal_slots'] = sel.xpath().extract()
+                item['Aantal_pins'] = sel.xpath().extract()
+                item['Aantal_6_pins'] = sel.xpath().extract()
+                item['Aantal_8_pins'] = sel.xpath().extract()
+                item['Stroomverbruik'] = sel.xpath().extract()
+                item['Type_koeling'] = sel.xpath().extract()
+                item['Link_Interface'] = sel.xpath().extract()
+                item['EAN'] = sel.xpath().extract()
+                item['SKU'] = sel.xpath().extract()
                 print "Videokaart"
             elif "Geheugen intern" in category:
                 print "Geheugen"
@@ -53,4 +90,3 @@ class TweakersSpider(CrawlSpider):
                 print "Processorkoeling"
             elif "Barebones" in category:
                 print "Barebone"
-
