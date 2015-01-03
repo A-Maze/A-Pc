2f6472a79e93e53715bd95cd398fbb766f05a25f
rikvanderwerf <Rikvanderwerf@outlook.com>
2015-01-03 14:45:35 +0100

    Update tests.py

--- Website/pcbuilder/tests.py
+++ Website/pcbuilder/tests.py
@@ -10,7 +10,7 @@ class testCases(TestCase):
 	# Test of de homepagina correct wordt geladen
 	def test(self):
 		webdriver.Remote(
-   			command_executor='http://127.0.0.1:4444/wd/hub',
+   			command_executor='http://127.0.0.1:4444',
    			desired_capabilities=DesiredCapabilities.FIREFOX)
 		driver.get('http://127.0.01:8000/')
 		driver.quit()
