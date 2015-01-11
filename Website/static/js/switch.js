fcb2c1c9da50a2116a6d1432c53a5c8f58b75001
Melloww <Melloww@github.com>
2015-01-11 23:38:43 +0100

    automatic compile front end

--- Website/static/js/switch.js
+++ Website/static/js/switch.js
@@ -9,6 +9,10 @@ function show(which){
 	};
 };
 
-function showDrops(){
-	
+//toggles the dropdown menus
+function showDrops(e){
+	//finds the sibling of the element that was clicked
+	var sibling = $(e).siblings('.panel-footer');
+	//finds the dropdown panel in that sibling and toggles it
+	sibling.children('.dropdown-panel').toggle('fast');
 }
